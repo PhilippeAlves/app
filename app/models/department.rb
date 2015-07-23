7a176c26006628d4cbbf6b69cf0a81da245fe7aa
Guilherme <guilhermefeitosa66@gmail.com>
2015-07-23 14:19:34 -0300

    defined relationship between models

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,3 +1,4 @@
 class Department < ActiveRecord::Base
   belongs_to :institution
+  has_many :floors, dependent: :destroy
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,6 @@
 class Room < ActiveRecord::Base
   belongs_to :permission
   belongs_to :floor
+  has_many :properties, dependent: :destroy
+  has_many :check_points, dependent: :destroy
 end
