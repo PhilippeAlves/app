755e6406390648b86ecb7ebf2a38571ed8c43626
Philippe <philippealves77@gmail.com>
2015-07-29 16:10:24 -0300

    remove reference from  floor to department and added relation from department to room

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,7 @@
 class Room < ActiveRecord::Base
   belongs_to :permission
   belongs_to :floor
+  belongs_to :department
   has_many :properties, dependent: :destroy
   has_many :check_points, dependent: :destroy
 end
